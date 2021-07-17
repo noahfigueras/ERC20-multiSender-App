56284724afb114fe1290fc86945490a1687025bb
noah figueras <noahfigueras@gmail.com>
2021-07-16 19:10:03 -0700

    Addded IERC20Metadata to MultiSender

--- contracts/MultiSend.sol
+++ contracts/MultiSend.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract MultiSender{
     
@@ -11,11 +12,11 @@ contract MultiSender{
     }
     
     //Returns total value of transaction transfer
-    function sum(uint[] memory amounts) internal pure returns(uint totalAmount){
+    function sum(uint[] memory amounts, uint8 decimals) internal pure returns(uint totalAmount){
         totalAmount = 0;
 
         for (uint i=0; i < amounts.length; i++) {
-            totalAmount += (amounts[i] * (10**18));
+            totalAmount += (amounts[i] * (10**decimals));
         }
     }
 
@@ -25,19 +26,21 @@ contract MultiSender{
          address _token
         ) external {
         IERC20 token = IERC20(address(_token));
+        uint8 decimals = IERC20Metadata(address(_token)).decimals();
+
         require(addrs.length == amounts.length, "Amount of addresses or transfer values are wrong");
-        require(token.allowance(msg.sender, address(this)) >= sum(amounts), "Allowance is less than amounts to transfer");
+        require(token.allowance(msg.sender, address(this)) >= sum(amounts, decimals), "Allowance is less than amounts to transfer");
 
         //Initialize token and transfer
         for (uint i=0; i < addrs.length; i++) {
             //send the specified amount to the recipients
-           _tokenTransfer(token, addrs[i], msg.sender, amounts[i]);
+           _tokenTransfer(token, addrs[i], msg.sender, amounts[i], decimals);
         }
    
     } 
     
     //Internal token Transfer
-    function _tokenTransfer(IERC20 token, address payable addrs, address sender, uint amount) internal {
-        token.transferFrom(sender, addrs, amount * (10**18));
+    function _tokenTransfer(IERC20 token, address payable addrs, address sender, uint amount, uint8 decimals) internal {
+        token.transferFrom(sender, addrs, amount * (10**decimals));
    } 
 }
